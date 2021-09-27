c09f226f25d1667c874cb58fe6f7b3ea194fd6d7
Sergey Derbush <sderbush@lineate.com>
2021-09-27 10:55:58 +0300

    add analytics build

--- gulpfile.js
+++ gulpfile.js
@@ -398,6 +398,7 @@ gulp.task(viewCoverage);
 gulp.task('coveralls', gulp.series('test-coverage', coveralls));
 
 gulp.task('build', gulp.series(clean, 'build-bundle-prod'));
+gulp.task('build-analytics', gulp.series(clean, 'build-bundle-prod'));
 gulp.task('build-postbid', gulp.series(escapePostbidConfig, buildPostbid));
 
 gulp.task('serve', gulp.series(clean, lint, gulp.parallel('build-bundle-dev', watch, test)));
