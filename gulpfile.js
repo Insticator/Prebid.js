1b6632348900d43c7fbc3fc21994bceb0dd6d2b3
Sergey Derbush <sderbush@lineate.com>
2021-09-29 01:04:20 +0300

    enable analytics by default

--- gulpfile.js
+++ gulpfile.js
@@ -400,6 +400,13 @@ gulp.task('coveralls', gulp.series('test-coverage', coveralls));
 gulp.task('build', gulp.series(clean, 'build-bundle-prod'));
 gulp.task('build-postbid', gulp.series(escapePostbidConfig, buildPostbid));
 
+function buildAnalytics() {
+  return gulp.src(['./build/dist/prebid.js', './analytics/init.js'])
+    .pipe(concat('prebid-analytics.js'))
+    .pipe(gulp.dest('build/dist'));
+}
+gulp.task('build-analytics', gulp.series('build', buildAnalytics));
+
 gulp.task('serve', gulp.series(clean, lint, gulp.parallel('build-bundle-dev', watch, test)));
 gulp.task('serve-fast', gulp.series(clean, gulp.parallel('build-bundle-dev', watch)));
 gulp.task('serve-fake', gulp.series(clean, gulp.parallel('build-bundle-dev', watch), injectFakeServerEndpointDev, test, startFakeServer));
