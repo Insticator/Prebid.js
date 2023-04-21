5a47ab11d2b06c4a84f53545dd1f1583ae7cd9c4
Robson Viana <robson@insticator.com>
2023-04-21 16:10:46 -0400

    Revert "Update insticatorBidAdapter_spec.js"
    This reverts commit 40742ffd38c7be7338d95667ca37bfe7b73b51f0.

--- test/spec/modules/insticatorBidAdapter_spec.js
+++ test/spec/modules/insticatorBidAdapter_spec.js
@@ -260,7 +260,7 @@ describe('InsticatorBidAdapter', function () {
       expect(data.regs.ext.gdpr).to.equal(1);
       expect(data.regs.ext.gdprConsentString).to.equal(bidderRequest.gdprConsent.consentString);
       expect(data.user).to.be.an('object');
-      expect(data.user.buyeruid).to.equal(USER_ID_DUMMY_VALUE);
+      expect(data.user.id).to.equal(USER_ID_DUMMY_VALUE);
       expect(data.user.ext).to.have.property('eids');
       expect(data.user.ext.eids).to.deep.equal([
         {
