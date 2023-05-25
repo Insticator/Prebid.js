e07fe37b051c8320f673129f0c5bb7b91d3d242c
Bi Yoo <95636458+insticator-biY@users.noreply.github.com>
2023-05-25 12:12:26 -0500

    set impression type using header bidding wrapper's API (#66)
    * set impression type using header bidding wrapper's API
    
    * linter

--- modules/insticatorBidAdapter.js
+++ modules/insticatorBidAdapter.js
@@ -73,8 +73,24 @@ function buildImpression(bidRequest) {
     insticator: {
       adUnitId: bidRequest.params.adUnitId,
       adUnitName: bidRequest.params.adUnitName,
-      impressionType: bidRequest.params.impressionType
     },
+  }
+
+  /**
+   * set impression type using header bidding wrapper's API
+   * this is Insticator header bidding wrapper specific
+   */
+  // eslint-disable-next-line no-undef
+  if (bidRequest.adUnitCode && Insticator.getAdUnitStates) {
+    try {
+      // eslint-disable-next-line no-undef
+      const adUnits = Insticator.getAdUnitStates();
+      const adUnit = adUnits[bidRequest.adUnitCode]
+      // eslint-disable-next-line no-undef
+      if (adUnit) ext.insticator.impressionType = adUnit.timesRefreshed > 0 ? adUnit.refreshType : 'il';
+    } catch (e) {
+      console.warn(e)
+    }
   }
 
   const sizes =
