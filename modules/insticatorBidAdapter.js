8ef91f1ff03b016e6bd5c55654cc04aa3f081ad3
Bi Yoo <95636458+insticator-biY@users.noreply.github.com>
2023-09-21 13:54:04 -0500

    add seat info when building bids (#74)
    * add seat info when building bids
    
    * extract bidder identifier from seat
    
    * Revert "extract bidder identifier from seat"
    
    This reverts commit 87075f0b57ee44c8e9ed67b3bfc7d17e9abdd7b4.
    
    * re - extract bidder identifier from seat

--- modules/insticatorBidAdapter.js
+++ modules/insticatorBidAdapter.js
@@ -235,7 +235,7 @@ function buildRequest(validBidRequests, bidderRequest) {
   return req;
 }
 
-function buildBid(bid, bidderRequest) {
+function buildBid(bid, bidderRequest, seat) {
   const originalBid = find(bidderRequest.bids, (b) => b.bidId === bid.impid);
   let meta = {}
 
@@ -245,6 +245,11 @@ function buildBid(bid, bidderRequest) {
 
   if (bid.adomain) {
     meta.advertiserDomains = bid.adomain
+  }
+  // seatbid.seat = `${bidRequesterId}_${seatbid.seat || ''}`;
+  if (seat && typeof seat === 'string') {
+    const bidderId = seat.split('_')[0]
+    meta.seat = bidderId
   }
 
   return {
@@ -264,7 +269,7 @@ function buildBid(bid, bidderRequest) {
 }
 
 function buildBidSet(seatbid, bidderRequest) {
-  return seatbid.bid.map((bid) => buildBid(bid, bidderRequest));
+  return seatbid.bid.map((bid) => buildBid(bid, bidderRequest, seatbid.seat));
 }
 
 function validateSize(size) {
